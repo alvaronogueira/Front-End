c1e5ef0079e31eba61429cdc3f256271e04dc9d7
alvaronogueira <alvaronogueira29@gmail.com>
2022-06-27 13:00:10 -0300

    GameMania

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+$ (document).ready(function () {
+
+    $("#barra").click(function () {
+        if($("#menu").hasClass("menu-ativo")) {
+            $("#menu").removeClass("menu-ativo")
+        }else{
+            $("#menu").addClass("menu-ativo")
+        }
+
+    })
+
+})
+
 let menu = document.getElementById("menu")
 
 function meuMenu() {
